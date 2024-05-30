351e07e16fb8e68d0f7b76b08318636f0207e466
SeiichiN <billie175@gmail.com>
2024-05-30 16:28:56 +0900

    5/29 少しの修正

--- chap05/src/ex5_2/Main.java
+++ chap05/src/ex5_2/Main.java
@@ -3,24 +3,24 @@ package ex5_2;
 public class Main {
 	
 	// 練習 5-3
-	public static void email(String address, String text) {
+	public static void email(String title, String text) {
 		System.out.println
-		  (address + "に、以下のメールを送信しました");
-		System.out.println("件名：無題");
+		  ("abc@def.hi に、以下のメールを送信しました");
+		System.out.println("件名：" + title);
 		System.out.println("本文：" + text);
 	}
 
 	public static void email(String title, String address, String text) {
-		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println(address + "に送信しました");
 		System.out.println("件名：" + title);
 		System.out.println("本文：" + text);
 	}
 	
 	public static void main(String[] args) {
-		// String subject = "業務連絡";
+		String subject = "業務連絡";
 		String address = "abc@efg.hi";
 		String body = "明日お休みをいただけませんか？";
-		email(address, body);
+		email(subject, address, body);
 	}
 
 }
