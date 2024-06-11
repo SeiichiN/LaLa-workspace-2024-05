0c1f31bcdd244d502ab946f4a9a1c5a90c2bcc28
SeiichiN <billie175@gmail.com>
2024-06-11 09:19:09 +0900

    Fightクラスで戦うことにした

--- game/src/main/Fight.java
+++ game/src/main/Fight.java
@@ -1,43 +1,62 @@
 package main;
 
+import java.util.Random;
+import java.util.Scanner;
+
 import rpg.humans.Human;
 import rpg.monsters.Monster;
 
 public class Fight {
-	private Human human;
-	private Monster monster;
-	private boolean end;
+	private Monster[] monsters = new Monster[2];
+	private Human[] humans = new Human[2];
 	
-	public Fight() {
-		this.end = false;
+	public Fight(Human[] humans, Monster[] monsters) {
+		this.humans = humans;
+		this.monsters = monsters;
 	}
 
-	public void start() {
-		while (this.end == false) {
-			monster.attack(human);
-			if (human.getHp() <= 0) {
-				this.end = true;
+	public void play() {
+		while (true) {
+			System.out.println("1:戦う 2:逃げる 9:終了する 他:戦う");
+			int num = new Scanner(System.in).nextInt();
+			if (num == 2 || num == 9) { break; }
+			for (Human h : humans) {
+				System.out.println(h.getName() + "の攻撃");
+				Monster m = selectMonster(monsters);
+				h.attack(m);
 			}
-			human.attack(monster);
-			if (monster.getHp() <= 0) {
-				this.end = true;
+			for (Monster m : monsters) {
+				Human h = selectHuman(humans);
+				m.attack(h);
 			}
+			printStatus(monsters, humans);
+		}
+		System.out.println("お疲れ様");
+	}
+	
+	private void printStatus(Monster[] monsters, Human[] humans) {
+		for (Monster m : monsters) {
+			System.out.println(m);
+		}
+		for (Human h : humans) {
+			System.out.println(h);
 		}
 	}
-	public Human getHuman() {
-		return human;
-	}
-
-	public void setHuman(Human human) {
-		this.human = human;
-	}
-
-	public Monster getMonster() {
-		return monster;
-	}
-
-	public void setMonster(Monster monster) {
-		this.monster = monster;
-	}
 	
+	private Human selectHuman(Human[] humans) {
+		int num = new Random().nextInt(2);
+		return humans[num];
+	}
+
+	private Monster selectMonster(Monster[] monsters) {
+		System.out.println("どのモンスターと戦いますか？");
+		for (int i = 0; i < monsters.length; i++) {
+			if (monsters[i].isLiving()) {
+				System.out.print(i + ":" + monsters[i].getType() + " ");
+			}
+		}
+		System.out.print("> ");
+		int no = new Scanner(System.in).nextInt();
+		return monsters[no];
+	}
 }
--- game/src/main/Main.java
+++ game/src/main/Main.java
@@ -1,8 +1,5 @@
 package main;
 
-import java.util.Random;
-import java.util.Scanner;
-
 import rpg.humans.Hero;
 import rpg.humans.Human;
 import rpg.humans.Wizard;
@@ -19,47 +16,9 @@ public class Main {
 		Human[] humans = new Human[2];
 		humans[0] = new Hero("ミナト");
 		humans[1] = new Wizard("アサカ");
-		while (true) {
-			System.out.println("1:戦う 2:逃げる 9:終了する 他:戦う");
-			int num = new Scanner(System.in).nextInt();
-			if (num == 2 || num == 9) { break; }
-			for (Human h : humans) {
-				System.out.println(h.getName() + "の攻撃");
-				Monster m = selectMonster(monsters);
-				h.attack(m);
-			}
-			for (Monster m : monsters) {
-				Human h = selectHuman(humans);
-				m.attack(h);
-			}
-			printStatus(monsters, humans);
-		}
-		System.out.println("お疲れ様");
+		
+		Fight fight = new Fight(humans, monsters);
+		fight.play();
 	}
 	
-	public static void printStatus(Monster[] monsters, Human[] humans) {
-		for (Monster m : monsters) {
-			System.out.println(m);
-		}
-		for (Human h : humans) {
-			System.out.println(h);
-		}
-	}
-	
-	public static Human selectHuman(Human[] humans) {
-		int num = new Random().nextInt(2);
-		return humans[num];
-	}
-
-	public static Monster selectMonster(Monster[] monsters) {
-		System.out.println("どのモンスターと戦いますか？");
-		for (int i = 0; i < monsters.length; i++) {
-			if (monsters[i].isLiving()) {
-				System.out.print(i + ":" + monsters[i].getType() + " ");
-			}
-		}
-		System.out.print("> ");
-		int no = new Scanner(System.in).nextInt();
-		return monsters[no];
-	}
 }
