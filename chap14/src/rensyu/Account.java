bc40a715d008131306816e345f90f4182cae6cf8
SeiichiN <billie175@gmail.com>
2024-06-12 09:25:53 +0900

    練習問題14

--- chap14/src/rensyu/Account.java
+++ chap14/src/rensyu/Account.java
@@ -11,4 +11,17 @@ public class Account {
 	public String toString() {
 		return "\\" + this.balance + "(口座番号@" + this.accountNumber + ")";
 	}
+	
+	public boolean equals(Object o) {
+		if (o == this) { return true; }
+		if (o instanceof Account) {
+			Account a = (Account) o;
+			String a1 = a.accountNumber.trim();
+			String a2 = this.accountNumber.trim();
+			if (a1.equals(a2)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- chap14/src/rensyu/Main.java
+++ chap14/src/rensyu/Main.java
@@ -3,8 +3,14 @@ package rensyu;
 public class Main {
 
 	public static void main(String[] args) {
-			Account a = new Account("4649", 1592);
-			System.out.println(a);
+			Account a1 = new Account("4649", 1592);
+			System.out.println(a1);
+			Account a2 = new Account(" 4649", 2923);
+			if (a1.equals(a2)) {
+				System.out.println("同じ");
+			} else {
+				System.out.println("違う");
+			}
 
 	}
 
