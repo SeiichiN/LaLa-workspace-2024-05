98a3323029bbeca3489138f1d3f4669a3972518f
SeiichiN <billie175@gmail.com>
2024-06-18 09:36:40 +0900

    DateUtil.java modified. and 誕生日.txt add.

--- date_app/src/main2/DateUtil.java
+++ date_app/src/main2/DateUtil.java
@@ -2,29 +2,26 @@ package main2;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.format.ResolverStyle;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class DateUtil {
 	public LocalDate inputDate() {
 		Scanner scan = new Scanner(System.in);
 		LocalDate ld = null;
-		try {
-			System.out.print("西暦 > ");
-			int year = Integer.parseInt(scan.nextLine());
-			System.out.print("月 > ");
-			int month = Integer.parseInt(scan.nextLine());
-			System.out.print("日 > ");
-			int day = Integer.parseInt(scan.nextLine());
-			ld = LocalDate.of(year, month, day);
-		} catch (NumberFormatException e) {
-			System.out.println("数字ではありません");
-		} catch (DateTimeException e) {
-			System.out.println("正しい日付ではありません");
+		while (ld == null) {
+			try {
+				System.out.print("西暦 > ");
+				int year = Integer.parseInt(scan.nextLine());
+				System.out.print("月 > ");
+				int month = Integer.parseInt(scan.nextLine());
+				System.out.print("日 > ");
+				int day = Integer.parseInt(scan.nextLine());
+				ld = LocalDate.of(year, month, day);
+			} catch (NumberFormatException e) {
+				System.out.println("数字ではありません");
+			} catch (DateTimeException e) {
+				System.out.println("正しい日付ではありません");
+			}
 		}
 		return ld;
 	}
