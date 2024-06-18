28d9624af963d8a0e5f0c48636d5cacd41d7bd5e
SeiichiN <billie175@gmail.com>
2024-06-18 10:00:58 +0900

    date_app modified.

--- date_app/src/main2/Main.java
+++ date_app/src/main2/Main.java
@@ -1,20 +1,12 @@
 package main2;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.format.ResolverStyle;
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		DateUtil dateUtil = new DateUtil();
-		LocalDate ld = null;
-		while (ld == null) {
-			System.out.println("日付を入力");
-			ld = dateUtil.inputDate();
-		}
+		LocalDate ld = dateUtil.inputDate();
 		System.out.println(ld);
 	}
 	
